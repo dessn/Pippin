04ac2c7d02ae3a38ad9e217c2534450eb8976740
samreay <samuelreay@gmail.com>
2019-01-11 16:43:35 +1000

    Fixing hash problems.

--- pippin/snana_simulation.py
+++ pippin/snana_simulation.py
@@ -87,8 +87,6 @@ class SNANASimulation(ConfigBasedExecutable):
         for file in output_files:
             with open(file, "r") as f:
                 string_to_hash += f.read()
-            self.logger.warn(file)
-            self.logger.warn(hashlib.sha256(string_to_hash.encode('utf-8')).hexdigest())
         new_hash = hashlib.sha256(string_to_hash.encode('utf-8')).hexdigest()
         self.logger.debug(f"Current hash set to {new_hash}")
         regenerate = old_hash is None or old_hash != new_hash
