636fe16fd0dbc1530bc72d676983c09465ca0361
samreay <samuelreay@gmail.com>
2019-01-14 16:33:12 +1000

    More

--- pippin/snana_fit.py
+++ pippin/snana_fit.py
@@ -75,7 +75,8 @@ class SNANALightCurveFit(ConfigBasedExecutable):
         logging_file = self.config_path.replace(".nml", ".nml_log")
         with open(logging_file, "w") as f:
             # TODO: Add queue to config and run
-            subprocess.run(["split_and_fit.pl", self.config_path, "NOPROMPT"], stdout=f, stderr=subprocess.STDOUT, cwd=self.output_dir)
+            # subprocess.run(["split_and_fit.pl", self.config_path, "NOPROMPT"], stdout=f, stderr=subprocess.STDOUT, cwd=self.output_dir)
+            pass
         return False
         self.logger.info(f"Light curve fitting outputting to {logging_file}")
         done_file = f"{self.output_dir}/SPLIT_JOBS_LCFIT.tar.gz"
