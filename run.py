a531c772a85333cefd293cea9a4dff072ac01736
Samuel Hinton <samuelreay@gmail.com>
2019-01-16 16:22:07 +1000

    Specifying verbosity

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # Set up command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("config", help="the name of the yml config file to run. For example: configs/default.yml")
-    parser.add_argument("-v", "--verbose", help="increase output verbosity")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 
     args = parser.parse_args()
 
