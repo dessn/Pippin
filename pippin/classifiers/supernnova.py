72d399202b5b6ec81eac784732074fadf234413d
Samuel Hinton <samuelreay@gmail.com>
2019-03-20 19:22:15 +1000

    Updatin sntypes

--- pippin/classifiers/supernnova.py
+++ pippin/classifiers/supernnova.py
@@ -30,8 +30,8 @@ source ~/.bashrc
 conda activate {conda_env}
 module load cuda
 cd {path_to_supernnova}
-python run.py --data --sntypes {sntypes} --dump_dir {dump_dir} --raw_dir {photometry_dir} --fits_dir {fit_dir}
-python run.py --use_cuda --sntypes {sntypes} --dump_dir {dump_dir} {command}
+python run.py --data --sntypes '{sntypes}' --dump_dir {dump_dir} --raw_dir {photometry_dir} --fits_dir {fit_dir}
+python run.py --use_cuda --sntypes '{sntypes}' --dump_dir {dump_dir} {command}
         """
         self.conda_env = self.global_config["SuperNNova"]["conda_env"]
         self.path_to_supernnova = os.path.abspath(os.path.dirname(inspect.stack()[0][1]) + "/../../../" + self.global_config["SuperNNova"]["location"])
