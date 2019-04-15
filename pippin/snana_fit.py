a808a61086649cb39923b3d11c681f81e3dc27b6
Samuel Hinton <samuelreay@gmail.com>
2019-04-15 15:16:24 +1000

    Supernnova checking

--- pippin/snana_fit.py
+++ pippin/snana_fit.py
@@ -109,7 +109,7 @@ class SNANALightCurveFit(ConfigBasedExecutable):
         regenerate, new_hash = self.write_nml()
         if not regenerate:
             return new_hash
-        self.logger.info(f"Light curve fitting outputting to {logging_file}")
+        self.logger.info(f"Light curve fitting outputting to {self.logging_file}")
         with open(self.logging_file, "w") as f:
             # TODO: Add queue to config and run
             subprocess.run(["split_and_fit.pl", self.config_path, "NOPROMPT"], stdout=f, stderr=subprocess.STDOUT, cwd=self.output_dir)
