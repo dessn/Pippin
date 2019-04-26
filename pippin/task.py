2db3465c08586b8aad2a0c5f4a1adabbef29cdb8
Samuel Hinton <samuelreay@gmail.com>
2019-04-27 00:23:27 +1000

    More logging please

--- pippin/task.py
+++ pippin/task.py
@@ -84,9 +84,14 @@ class Task(ABC):
         if result in [Task.FINISHED_SUCCESS, Task.FINISHED_FAILURE]:
             if os.path.exists(self.done_file):
                 self.end_time = os.path.getmtime(self.done_file)
+
                 if self.start_time is None and os.path.exists(self.hash_file):
                     self.start_time = os.path.getmtime(self.hash_file)
                 self.wall_time = int(self.end_time - self.start_time + 0.5)  # round up
+                print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ", self)
+                print(datetime.datetime.fromtimestamp(self.start_time).strftime("%A, %B %d, %Y %I:%M:%S"))
+                print(datetime.datetime.fromtimestamp(self.end_time).strftime("%A, %B %d, %Y %I:%M:%S"))
+                print(self.wall_time)
                 self.logger.info(f"Task finished with wall time {self.get_wall_time_str()}")
         return result
 
