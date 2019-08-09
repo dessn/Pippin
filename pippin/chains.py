40e491d2fc09e5511c1e3e8ffe8df5abfb0e4d62
Samuel Hinton <samuelreay@gmail.com>
2019-08-09 17:16:53 -0400

    Adding task to analyse chains

---
+++ pippin/chains.py
@@ -0,0 +1,111 @@
+import inspect
+import shutil
+import subprocess
+import os
+
+from pippin.config import mkdirs, get_config
+from pippin.create_cov import CreateCov
+from pippin.task import Task
+
+
+class AnalyseChains(Task):  # TODO: Define the location of the output so we can run the lc fitting on it.
+    """ Smack the data into something that looks like the simulated data
+
+    CONFIGURATION
+    =============
+
+    COSMOMC:
+        label:
+            MASK_COSMOMC: mask  # partial match
+            OPTS:
+                BLIND: [omegam, w]  # Optional. Parameters to blind. Single or list. omegam, omegal, w, wa, nu, etc
+                COVOPTS: [ALL, NOSYS] # Optional. Covopts to match. Single or list. Exact match.
+
+    OUTPUTS
+    =======
+
+
+
+    """
+    def __init__(self, name, output_dir, options, dependencies=None):
+        super().__init__(name, output_dir, dependencies=dependencies)
+        self.options = options
+        self.global_config = get_config()
+
+        self.logfile = os.path.join(self.output_dir, "output.log")
+        self.job_name = f"anaylyse_chains_{name}"
+        self.path_to_code = os.path.dirname(inspect.stack()[0][1]) + "/external/plot_cosmomc.py"
+
+        self.covopts = options.get("COVOPTS")
+        if isinstance(self.covopts, str):
+            self.covopts = [self.covopts]
+
+        self.files = []
+        self.params = []
+        self.blind_params = options.get("BLIND")
+
+        # Assuming all deps are cosmomc tasks
+        for c in self.dependencies:
+            for covopt in c.output["covopts"]:
+                if covopt in self.covopts or self.covopts is None:
+                    self.files.append(c.output["base_dict"][covopt])
+                    self.params += c.output["cosmology_params"]
+        self.params = list(set(self.params))
+
+        self.slurm = """#!/bin/bash
+#SBATCH --job-name={job_name}
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=broadwl
+#SBATCH --output={log_file}
+#SBATCH --account=pi-rkessler
+#SBATCH --mem=10GB
+
+cd {output_dir}
+python {path_to_code} {files} {name} {blind} {done_file} {params}
+"""
+
+    def _check_completion(self, squeue):
+        if os.path.exists(self.done_file):
+            self.logger.debug(f"Done file found at f{self.done_file}")
+            with open(self.done_file) as f:
+                if "FAILURE" in f.read():
+                    self.logger.error(f"Done file reported failure. Check output log {self.logfile}")
+                    return Task.FINISHED_FAILURE
+                else:
+                    return Task.FINISHED_SUCCESS
+        return 1
+
+    def _run(self, force_refresh):
+
+        format_dict = {
+            "job_name": self.job_name,
+            "log_file": self.logfile,
+            "done_file": "-d " + self.done_file,
+            "path_to_code": self.path_to_code,
+            "output_dir": self.output_dir,
+            "files": " ".join(self.files),
+            "name": "-n " + self.name,
+            "blind": ("-b " + " ".join(self.blind_params)) if self.blind_params else "",
+            "params": "-p " + " ".join(self.params)
+        }
+        final_slurm = self.slurm.format(**format_dict)
+
+        new_hash = self.get_hash_from_string(final_slurm)
+        old_hash = self.get_old_hash()
+
+        if force_refresh or new_hash != old_hash:
+            self.logger.debug("Regenerating and launching task")
+            shutil.rmtree(self.output_dir, ignore_errors=True)
+            mkdirs(self.output_dir)
+            self.save_new_hash(new_hash)
+            slurm_output_file = os.path.join(self.output_dir, "slurm.job")
+            with open(slurm_output_file, "w") as f:
+                f.write(final_slurm)
+            self.logger.info(f"Submitting batch job for data prep")
+            subprocess.run(["sbatch", slurm_output_file], cwd=self.output_dir)
+        else:
+            self.logger.info("Hash check passed, not rerunning")
+        return True
---
+++ pippin/external/plot_cosmomc.py
@@ -0,0 +1,103 @@
+import numpy as np
+from chainconsumer import ChainConsumer
+import pandas as pd
+import argparse
+import os
+import logging
+
+
+def load_params(file):
+    assert os.path.exists(file), f"Paramnames file {file} does not exist"
+    names, labels = [], []
+    with open(file) as f:
+        for line in f.read().splitlines():
+            n, l = line.split(maxsplit=1)
+            names.append(n.replace("*", ""))
+            labels.append(l)
+    return names, labels
+
+
+def load_chains(files, all_cols, use_cols=None):
+    header = ["weights", "likelihood"] + all_cols
+    data = [pd.read_csv(f, delim_whitespace=True, header=None, names=header) for f in files]
+    combined = pd.concat(data)
+    if use_cols is None:
+        use_cols = all_cols
+    weights = combined["weights"].values
+    likelihood = combined["likelihood"].values
+    chain = combined[use_cols].values
+    return weights, likelihood, chain
+
+
+def get_chain_files(basename):
+    folder = os.path.dirname(basename)
+    logging.info(f"Looking for chains in folder {folder}")
+    base = os.path.basename(basename)
+    files = [os.path.join(folder, f) for f in sorted(os.listdir(folder)) if base in f and f.endswith(".txt")]
+    assert len(files) > 0, f"No chain files found for {os.path.join(folder, basename)}"
+    return files
+
+
+def setup_logging():
+    fmt = "[%(levelname)8s |%(filename)21s:%(lineno)3d]   %(message)s"
+    logging.basicConfig(level=logging.INFO, format=fmt)
+
+
+def blind(chain, names, columns_to_blind):
+    np.random.seed(123)
+    for i, c in enumerate(columns_to_blind):
+        logging.info(f"Blinding column {c}")
+        try:
+            index = names.index(c)
+            scale = np.random.normal(loc=1, scale=0.1, size=1000)[321 + i]
+            chain[:, index] *= scale
+        except ValueError as e:
+            logging.error(f"Cannot find blinding column {c} in list of names {names}")
+            raise e
+
+
+def get_arguments():
+    # Set up command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("basename", help="The base to use for paramnames Eg /path/SN_CMB_OMW_ALL", nargs="+", type=str)
+    parser.add_argument("-p", "--params", help="Param names to plot", nargs="*", type=str, default=["omegam", "w"])
+    parser.add_argument("-n", "--name", help="Name of plot: eg name_without_extension", type=str, default="output")
+    parser.add_argument("-b", "--blind", help="Blind these parameters", nargs="*", type=str, default=None)
+    parser.add_argument("-d", "--donefile", help="Path of done file", type=str, default="done.txt")
+    return parser.parse_args()
+
+
+def get_output(basename, args):
+    param_file = os.path.join(basename) + ".paramnames"
+    chain_files = get_chain_files(basename)
+    names, labels = load_params(param_file)
+    weights, likelihood, chain = load_chains(chain_files, names, args.params)
+    if args.blind:
+        blind(chain, args.params or names, args.blind)
+    labels = [f"${l}$" + (" Blinded" if u in args.blind else "") for u in args.params for l, n in zip(labels, names) if n == u]
+    logging.info(f"Chain for {basename} has shape {chain.shape}")
+    logging.info(f"Labels for {basename} are {labels}")
+    return weights, likelihood, labels, chain
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    try:
+        setup_logging()
+        c = ChainConsumer()
+        for basename in args.basename:
+            weights, likelihood, labels, chain = get_output(basename, args)
+            name = os.path.basename(basename).replace("_", " ")
+            c.add_chain(chain, weights=weights, parameters=labels, name=name, posterior=likelihood)
+
+        # Write all our glorious output
+        c.analysis.get_latex_table(filename=args.name + "_params.txt")
+        c.plotter.plot(filename=args.name + ".png", figsize=1.5)
+        c.plotter.plot_summary(filename=args.name + "_summary.png", errorbar=True)
+        c.plotter.plot_walks(filename=args.name + "_walks.png")
+
+        with open(args.donefile, "w") as f:
+            f.write("SUCCESS")
+    except Exception as e:
+        with open(args.donefile, "w") as f:
+            f.write("FAILURE")
