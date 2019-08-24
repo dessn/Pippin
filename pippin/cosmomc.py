76c6ac96f05b6bd4437d2359069fba48c24ecfa4
Samuel Hinton <samuelreay@gmail.com>
2019-08-24 13:14:19 +1000

    Lets try again shall we

--- pippin/cosmomc.py
+++ pippin/cosmomc.py
@@ -52,7 +52,7 @@ class CosmoMC(Task):  # TODO: Define the location of the output so we can run th
         self.num_jobs = len(self.covopts)
         self.ini_prefix = options.get("INI")
         self.num_walkers = options.get("NUM_WALKERS", 4)
-        self.chain_dir = os.path.join(self.output_dir, "chains")
+        self.chain_dir = os.path.join(self.output_dir, "chains/")
 
         self.labels = [self.name + "_" + c for c in self.covopts]
         self.ini_files = [f"{self.ini_prefix}_{num}.ini" for num in self.covopts_numbers]
