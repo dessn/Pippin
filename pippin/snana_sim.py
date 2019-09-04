67277ea5461d0222fd4adcf6e29e5f8be5afae19
Samuel Hinton <samuelreay@gmail.com>
2019-09-04 13:17:33 +1000

    Whats wrong with sed now

--- pippin/snana_sim.py
+++ pippin/snana_sim.py
@@ -94,7 +94,9 @@ class SNANASimulation(ConfigBasedExecutable):
                                 base = os.path.basename(include_file)
                                 output = os.path.join(temp_dir, base)
                                 input = os.path.join(self.data_dir, include_file)
-                                subprocess.run(["sed", f"'s|{include_file}|{base}|g'", f"<{input}", f">{output}"])
+                                cmds = ["sed", f"'s|{include_file}|{base}|g'", f"<{input}", f">{output}"]
+                                self.logger.info(f"Running sed command: {''.join(cmds)}")
+                                subprocess.run(cmds)
                                 # shutil.copy(self.data_dir + include_file, temp_dir)
 
                             fs.append(os.path.join(temp_dir, os.path.basename(include_file)))
