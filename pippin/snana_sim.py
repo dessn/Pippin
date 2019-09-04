3fb99d24e833a94d4ab9744889bbdcb5a4275693
Samuel Hinton <samuelreay@gmail.com>
2019-09-04 13:13:41 +1000

    Invoking the hacky power of sed

--- pippin/snana_sim.py
+++ pippin/snana_sim.py
@@ -89,7 +89,14 @@ class SNANASimulation(ConfigBasedExecutable):
                             if include_file.startswith("/"):
                                 shutil.copy(include_file, temp_dir)
                             else:
-                                shutil.copy(self.data_dir + include_file, temp_dir)
+                                # Dont copy it over, we need to sed it to update the INPUT_FILE_INCLUDE to be relative
+                                # Ah crap, this will only work for a single include.
+                                base = os.path.basename(include_file)
+                                output = os.path.join(temp_dir, base)
+                                input = os.path.join(self.data_dir, include_file)
+                                subprocess.run(["sed", f"s/{include_file}/{base}/g", f"<{input}", f">{output}"])
+                                # shutil.copy(self.data_dir + include_file, temp_dir)
+
                             fs.append(os.path.join(temp_dir, os.path.basename(include_file)))
 
         # Write the primary input file
