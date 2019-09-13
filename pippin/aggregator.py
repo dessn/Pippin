fdb55c56367590bcb902148946245dc5a941a02d
Samuel Hinton <samuelreay@gmail.com>
2019-09-13 21:24:15 +1000

    More debug output

--- pippin/aggregator.py
+++ pippin/aggregator.py
@@ -142,6 +142,7 @@ class Aggregator(Task):
             df = pd.merge(df, type_df, on=self.id, how="left")
 
             types = self.get_underlying_sim_task().output["types_dict"]
+            self.logger.debug(f"Types are {types}")
             ia = df["SNTYPE"].apply(lambda y: True if y in types["IA"] else (False if y in types["NONIA"] else np.nan))
             df["IA"] = ia
 
