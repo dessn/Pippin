79fefe4d30c7970c22f0e8b4fc6185aa9c699828
Samuel Hinton <samuelreay@gmail.com>
2019-09-16 19:53:15 +1000

    Decreasing batch_size

--- pippin/classifiers/supernnova.py
+++ pippin/classifiers/supernnova.py
@@ -66,7 +66,7 @@ python run.py --data --sntypes '{sntypes}' --dump_dir {dump_dir} --raw_dir {phot
 if [ $? -ne 0 ]; then
     echo FAILURE > {done_file2}
 fi
-python run.py --use_cuda {cyclic} --sntypes '{sntypes}' --done_file {done_file} --dump_dir {dump_dir} {cyclic} {variant} {model} {phot} {command}
+python run.py --use_cuda {cyclic} --sntypes '{sntypes}' --done_file {done_file} --batch_size 50 --dump_dir {dump_dir} {cyclic} {variant} {model} {phot} {command}
 if [ $? -eq 0 ]; then
     echo SUCCESS > {done_file2}
 else
