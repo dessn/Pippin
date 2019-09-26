be4680fcec7a28fe4a8f1b021a7ea37d1f1e1d74
Samuel Hinton <samuelreay@gmail.com>
2019-09-26 23:14:36 +1000

    Better analyse with prior

--- pippin/external/plot_cosmomc.py
+++ pippin/external/plot_cosmomc.py
@@ -53,7 +53,7 @@ def get_chain_files(basename):
 def setup_logging():
     fmt = "[%(levelname)8s |%(filename)21s:%(lineno)3d]   %(message)s"
     handler = logging.StreamHandler(sys.stdout)
-    logging.basicConfig(level=logging.DEBUG, format=fmt, handlers=[handler])
+    logging.basicConfig(level=logging.DEBUG, format=fmt, handlers=[handler, logging.FileHandler("plot_logger.log")])
 
 
 def blind(chain, names, columns_to_blind, index=0):
@@ -131,9 +131,9 @@ def load_output(basename):
 
 
 if __name__ == "__main__":
+    setup_logging()
     args = get_arguments()
     try:
-        setup_logging()
         logging.info("Creating chain consumer object")
         c = ChainConsumer()
         do_full = False
