cf34afcd323402a78bc00d9bd976946d48db4c1d
Samuel Hinton <samuelreay@gmail.com>
2019-09-26 23:46:00 +1000

    Unity fix

--- pippin/classifiers/unity.py
+++ pippin/classifiers/unity.py
@@ -58,7 +58,7 @@ class UnityClassifier(Classifier):
         if new_hash:
             mkdirs(self.output_dir)
             input = self.get_fit_dependency()
-            fitres_file = input["fitres_file"][self.index]
+            fitres_file = os.path.abspath(os.path.join(input["fitres_dirs"][self.index], input["fitopt_map"]["DEFAULT"]))
             self.logger.debug(f"Looking for {fitres_file}")
             if not os.path.exists(fitres_file):
                 self.logger.error(f"FITRES file could not be found at {fitres_file}, classifer has nothing to work with")
