f4425dcfc8093faac7145427ffd6767b9694a408
Samuel Hinton <samuelreay@gmail.com>
2019-10-12 13:24:06 +1000

    Updating NN classifier to a new python version

--- pippin/classifiers/nearest_neighbor_python.py
+++ pippin/classifiers/nearest_neighbor_python.py
@@ -42,7 +42,7 @@ class NearestNeighborPyClassifier(Classifier):
 
         self.path_to_classifier = os.path.dirname(inspect.stack()[0][1])
         self.job_base_name = os.path.basename(output_dir)
-        self.features = options.get("FEATURES", "zHD x1 c cERR x1ERR mBERR COV_x1_c COV_x1_x0 COV_c_x0 PKMJDERR")
+        self.features = options.get("FEATURES", "zHD x1 c cERR x1ERR COV_x1_c COV_x1_x0 COV_c_x0 PKMJDERR")
         self.model_pk_file = "model.pkl"
         self.output_pk_file = os.path.join(self.output_dir, self.model_pk_file)
         self.predictions_filename = os.path.join(self.output_dir, "predictions.csv")
