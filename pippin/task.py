068346d088ba3372f63aee5e4a076c7592a9a967
Samuel Hinton <samuelreay@gmail.com>
2019-10-18 17:19:00 +1000

    Let's be conservative here

--- pippin/task.py
+++ pippin/task.py
@@ -26,7 +26,7 @@ class Task(ABC):
         self.stage = None
         self.fresh_run = True
         self.num_empty = 0
-        self.num_empty_threshold = 10
+        self.num_empty_threshold = 25
 
     def check_for_job(self, squeue, match):
         if squeue is None:
