8a037f66fb7da701a7b0d6829f32b7a09ec25abc
Samuel Hinton <samuelreay@gmail.com>
2020-01-30 16:47:17 +1000

    Testing LCFIT as well

--- pippin/snana_sim.py
+++ pippin/snana_sim.py
@@ -175,7 +175,9 @@ class SNANASimulation(ConfigBasedExecutable):
                             if include_file.startswith("/"):
                                 shutil.copy(include_file, temp_dir)
                             else:
+                                print("AAAAAAAAAAA ", include_file)
                                 include_file = get_data_loc(self.data_dir, include_file)
+                                print("BBBBBBBBBBB ", include_file)
                                 # Dont copy it over, we need to sed it to update the INPUT_FILE_INCLUDE to be relative
                                 # Ah crap, this will only work for a single include.
                                 base = os.path.basename(include_file)
