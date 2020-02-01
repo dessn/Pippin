907d590af6986fed800f7017f77475d97100b3b2
Samuel Hinton <samuelreay@gmail.com>
2020-02-01 15:19:59 +1000

    Temporarily searching for "ERROR: " in the input_log file for simulations

--- pippin/snana_sim.py
+++ pippin/snana_sim.py
@@ -246,7 +246,7 @@ class SNANASimulation(ConfigBasedExecutable):
         if self.logging_file is not None and os.path.exists(self.logging_file):
             with open(self.logging_file, "r") as f:
                 for line in f.read().splitlines():
-                    if "ABORT ON FATAL ERROR" in line:
+                    if "ABORT ON FATAL ERROR" in line or " ERROR: " in line:
                         self.logger.error(f"Fatal error in simulation. See {self.logging_file} for details.")
                         output_error = True
                     if output_error:
