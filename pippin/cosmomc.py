5c6a01af16de0a34be70e725f587542820ec5421
Samuel Hinton <samuelreay@gmail.com>
2020-02-03 08:27:34 +1000

    Adding debug text

--- pippin/cosmomc.py
+++ pippin/cosmomc.py
@@ -50,6 +50,7 @@ class CosmoMC(Task):  # TODO: Define the location of the output so we can run th
 
         self.create_cov_dep = self.get_dep(CreateCov)
         self.blind = self.create_cov_dep.output["blind"] if self.create_cov_dep is not None else self.options.get("BLIND", False)
+        print("AAA ", self.blind, type(self.blind))
         assert isinstance(self.blind, bool), "Blind should be set to a boolan value!"
         self.ini_prefix = options.get("INI")
         self.static = self.ini_prefix in ["cmb_omw", "cmb_omol"]
