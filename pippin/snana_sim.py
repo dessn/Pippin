5b63a728b9af871ace92d7925739a65c6ff57c74
Samuel Hinton <samuelreay@gmail.com>
2020-02-04 09:27:59 +1000

    Updating data dir to use defaults

--- pippin/snana_sim.py
+++ pippin/snana_sim.py
@@ -113,6 +113,9 @@ class SNANASimulation(ConfigBasedExecutable):
     def write_input(self, force_refresh):
         self.set_property("GENVERSION", self.genversion, assignment=": ", section_end="ENDLIST_GENVERSION")
         self.set_property("LOGDIR", os.path.basename(self.sim_log_dir), assignment=": ", section_end="ENDLIST_GENVERSION")
+        if len(self.data_dirs) > 1:
+            data_dir = self.data_dirs[0]
+            self.set_property("PATH_USER_INPUT", data_dir, assignment=": ", section_end="ENDLIST_GENVERSION")
         for k in self.config.keys():
             if k.upper() != "GLOBAL":
                 run_config = self.config[k]
