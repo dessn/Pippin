cc553fed386b1e7b3057059b34db54001f52ec37
Samuel Hinton <samuelreay@gmail.com>
2020-03-02 14:18:56 +1000

    Adding fitprob cut

--- pippin/external/compute_map.py
+++ pippin/external/compute_map.py
@@ -45,16 +45,16 @@ def parse_data(filename):
     mask_sbfluxcal = all_data["SBFLUXCAL"] > 0
     mask_5yr = all_data["MJD"] < 57850
     mask_nolowz_y1 = (all_data["MJD"] > 56800) | (all_data["zHD"] > 0.13)
-    mask_fitprob = all_data["FITPROB"] > 0.01
+    # mask_fitprob = all_data["FITPROB"] > 0.01
 
     # mask_ratio = all_data["RATIO"] > 0
-    mask = mask_errtest_high & mask_errtest_low & mask_sbfluxcal & mask_5yr & mask_nolowz_y1 & mask_fitprob  # & mask_fluxcalsim & mask_ratio
+    mask = mask_errtest_high & mask_errtest_low & mask_sbfluxcal & mask_5yr & mask_nolowz_y1  # & mask_fitprob  # & mask_fluxcalsim & mask_ratio
     print_drop(mask_errtest_high, "errtest < 10")
     print_drop(mask_errtest_low, "errtest >= 0.1")
     print_drop(mask_sbfluxcal, "SBFLUXCAL > 0")
     print_drop(mask_5yr, "MJD < 57850")
     print_drop(mask_nolowz_y1, "No Y1 z<0.13")
-    print_drop(mask_fitprob, "fitprob >0.01")
+    # print_drop(mask_fitprob, "fitprob >0.01")
     print_drop(mask, "Combined mask")
 
     all_data = all_data.loc[mask, :]
@@ -63,14 +63,14 @@ def parse_data(filename):
     print(f"Saving merged and thresholded data to {filename}")
     all_data.to_pickle(filename)
 
-    # Finally, lets get rid of massive outlier points. 15sigma should be good enough.
 
-
-def get_data(filename, cut):
+def get_data(filename, cut, cut_fitprob=0.0):
     df = pd.read_pickle(filename)
     mask2 = np.abs(df["DEVIATIONSIM"]) < cut
     print_drop(mask2, f"{cut} sigma outlier")
-    return df.loc[mask2, :]
+    mask_fitprob = df["FITPROB"] >= cut_fitprob
+    print_drop(mask_fitprob, f"FITPROB >= {cut_fitprob}")
+    return df.loc[mask2 & mask_fitprob, :]
 
 
 def rejstd(x, debug=False):
@@ -119,116 +119,117 @@ if not os.path.exists("fakes_obs.pkl"):
     print("Finished parsing")
 
 for cut in cuts:
-    print("Loading data")
-    df_f = get_data("fakes_obs.pkl", cut)
-    df_s = get_data("sim_obs.pkl", cut)
+    for cut_fitprob in [0.00, 0.01]:
+        print("Loading data")
+        df_f = get_data("fakes_obs.pkl", cut, cut_fitprob=cut_fitprob)
+        df_s = get_data("sim_obs.pkl", cut, cut_fitprob=cut_fitprob)
 
-    data = [
-        [("LOGSNR", np.arange(0, 3.1, 1)), ("SBMAG", np.arange(20, 30, 2)), ("PSF", np.array([1, 3, 5]))],
-        [("LOGSNR", np.arange(0, 3.1, 1)), ("SBMAG", np.arange(20, 30, 1))],
-        [("SBMAG", np.arange(20, 30, 1))],
-    ]
-    # data = [[("LOGSNR", np.arange(0, 3, 0.5)), ]]
-    # data = [[("SBMAG", np.arange(20, 30, 1))]]
+        data = [
+            [("LOGSNR", np.arange(0, 3.1, 1)), ("SBMAG", np.arange(20, 30, 2)), ("PSF", np.array([1, 3, 5]))],
+            [("LOGSNR", np.arange(0, 3.1, 1)), ("SBMAG", np.arange(20, 30, 1))],
+            [("SBMAG", np.arange(20, 30, 1))],
+        ]
+        # data = [[("LOGSNR", np.arange(0, 3, 0.5)), ]]
+        # data = [[("SBMAG", np.arange(20, 30, 1))]]
 
-    # Digitize our data into the right bins
-    print("Digitising")
-    for maps in data:
-        indices_f = []
-        indices_s = []
-        bcs = []
-        shape = []
-        for k, bins in maps:
-            if bins is None:
-                bins = np.linspace(df_f[k].quantile(0.001), df_f[k].quantile(0.999), 7)
-            bcs.append(0.5 * (bins[:-1] + bins[1:]))
-            shape.append(bcs[-1].size)
-            indices_f.append(np.digitize(df_f[k], bins=bins) - 1)
-            indices_s.append(np.digitize(df_s[k], bins=bins) - 1)
+        # Digitize our data into the right bins
+        print("Digitising")
+        for maps in data:
+            indices_f = []
+            indices_s = []
+            bcs = []
+            shape = []
+            for k, bins in maps:
+                if bins is None:
+                    bins = np.linspace(df_f[k].quantile(0.001), df_f[k].quantile(0.999), 7)
+                bcs.append(0.5 * (bins[:-1] + bins[1:]))
+                shape.append(bcs[-1].size)
+                indices_f.append(np.digitize(df_f[k], bins=bins) - 1)
+                indices_s.append(np.digitize(df_s[k], bins=bins) - 1)
 
-        data = {}
-        for field_name, color in zip(field_names, ("viridis", "magma")):
-            fields_list = ["E1", "E2", "S1", "S2", "C1", "C2", "X1", "X2"] if field_name == "SHALLOW" else ["C3", "X3"]
+            data = {}
+            for field_name, color in zip(field_names, ("viridis", "magma")):
+                fields_list = ["E1", "E2", "S1", "S2", "C1", "C2", "X1", "X2"] if field_name == "SHALLOW" else ["C3", "X3"]
 
-            for band_index, band in enumerate(bands):
-                print(f"Doing field {field_name} and band {band}")
+                for band_index, band in enumerate(bands):
+                    print(f"Doing field {field_name} and band {band}")
 
-                # Select down to field and band
-                mask_f = (df_f["IFILTOBS"] == band_index + 2) & (np.isin(df_f["FIELD"], fields_list))
-                mask_s = (df_s["IFILTOBS"] == band_index + 2) & (np.isin(df_s["FIELD"], fields_list))
-                df_f2 = df_f[mask_f]
-                df_s2 = df_s[mask_s]
-                indices_f2 = [i[mask_f] for i in indices_f]
-                indices_s2 = [i[mask_s] for i in indices_s]
+                    # Select down to field and band
+                    mask_f = (df_f["IFILTOBS"] == band_index + 2) & (np.isin(df_f["FIELD"], fields_list))
+                    mask_s = (df_s["IFILTOBS"] == band_index + 2) & (np.isin(df_s["FIELD"], fields_list))
+                    df_f2 = df_f[mask_f]
+                    df_s2 = df_s[mask_s]
+                    indices_f2 = [i[mask_f] for i in indices_f]
+                    indices_s2 = [i[mask_s] for i in indices_s]
 
-                # Create empty arrays to store everything
-                print("Creating arrays")
-                numerator = np.empty(shape)
-                denom_rms = np.empty(shape)
-                denom_ratio = np.empty(shape)
-                numerator[:] = np.nan
-                denom_rms[:] = np.nan
-                denom_ratio[:] = np.nan
+                    # Create empty arrays to store everything
+                    print("Creating arrays")
+                    numerator = np.empty(shape)
+                    denom_rms = np.empty(shape)
+                    denom_ratio = np.empty(shape)
+                    numerator[:] = np.nan
+                    denom_rms[:] = np.nan
+                    denom_ratio[:] = np.nan
 
-                # For each element in the list, do the math
-                for index, x in np.ndenumerate(numerator):
-                    print(index)
-                    mask_s = np.ones(df_s2.shape[0], dtype=bool)
-                    mask_f = np.ones(df_f2.shape[0], dtype=bool)
-                    for i, ind_f, ind_s in zip(index, indices_f2, indices_s2):
-                        mask_s &= ind_s == i
-                        mask_f &= ind_f == i
-                    df_s3 = df_s2[mask_s]
-                    df_f3 = df_f2[mask_f]
+                    # For each element in the list, do the math
+                    for index, x in np.ndenumerate(numerator):
+                        print(index)
+                        mask_s = np.ones(df_s2.shape[0], dtype=bool)
+                        mask_f = np.ones(df_f2.shape[0], dtype=bool)
+                        for i, ind_f, ind_s in zip(index, indices_f2, indices_s2):
+                            mask_s &= ind_s == i
+                            mask_f &= ind_f == i
+                        df_s3 = df_s2[mask_s]
+                        df_f3 = df_f2[mask_f]
 
-                    deviation_f = df_f3["DEVIATIONSIM"]
-                    deviation_s = df_s3["DEVIATIONSIM"]
-                    errratio = df_f3["ERRRATIO"]
+                        deviation_f = df_f3["DEVIATIONSIM"]
+                        deviation_s = df_s3["DEVIATIONSIM"]
+                        errratio = df_f3["ERRRATIO"]
 
-                    numerator[index] = rejstd(deviation_f)
-                    denom_rms[index] = rejstd(deviation_s)
-                    denom_ratio[index] = rejmean(errratio)
+                        numerator[index] = rejstd(deviation_f)
+                        denom_rms[index] = rejstd(deviation_s)
+                        denom_ratio[index] = rejmean(errratio)
 
-                # Construct final matrices
-                sims = numerator / denom_rms
-                fakes = numerator / denom_ratio
+                    # Construct final matrices
+                    sims = numerator / denom_rms
+                    fakes = numerator / denom_ratio
 
-                # Fill in missing values
-                print("Interpolating")
-                for i, (x, n) in enumerate([(sims, "SIM"), (fakes, "FAKES")]):
-                    ind = np.indices(x.shape).reshape((len(x.shape), -1))
-                    xx = x.flatten()
-                    non_nan = np.isfinite(xx)
+                    # Fill in missing values
+                    print("Interpolating")
+                    for i, (x, n) in enumerate([(sims, "SIM"), (fakes, "FAKES")]):
+                        ind = np.indices(x.shape).reshape((len(x.shape), -1))
+                        xx = x.flatten()
+                        non_nan = np.isfinite(xx)
 
-                    x2 = xx[non_nan]
-                    ind2 = ind[:, non_nan]
-                    print(ind2.shape, x2.shape, ind.shape)
-                    x = griddata(ind2.T, x2, ind.T, method="nearest").T.flatten().reshape((x.shape))
+                        x2 = xx[non_nan]
+                        ind2 = ind[:, non_nan]
+                        print(ind2.shape, x2.shape, ind.shape)
+                        x = griddata(ind2.T, x2, ind.T, method="nearest").T.flatten().reshape((x.shape))
 
-                    # Save to output
-                    data[n + field_name + band] = x
+                        # Save to output
+                        data[n + field_name + band] = x
 
-        # Create output files
-        for j, n in enumerate(["SIM", "FAKES"]):
-            output_string = []
-            output_string.append("DEFINE_FIELDGROUP: SHALLOW E1+E2+S1+S2+C1+C2+X1+X2")
-            output_string.append("DEFINE_FIELDGROUP: DEEP C3+X3\n")
-            names = [m[0] for m in maps]
-            for field in field_names:
-                for b in bands:
-                    d = data[n + field + b]
+            # Create output files
+            for j, n in enumerate(["SIM", "FAKES"]):
+                output_string = []
+                output_string.append("DEFINE_FIELDGROUP: SHALLOW E1+E2+S1+S2+C1+C2+X1+X2")
+                output_string.append("DEFINE_FIELDGROUP: DEEP C3+X3\n")
+                names = [m[0] for m in maps]
+                for field in field_names:
+                    for b in bands:
+                        d = data[n + field + b]
 
-                    output_string.append("MAPNAME: FLUXERR_SCALE")
-                    output_string.append(f"BAND: {b}  FIELD: {field}")
-                    output_string.append(f"VARNAMES:  {' '.join(names)} ERRSCALE")
-                    for inds in np.indices(d.shape).reshape((len(d.shape), -1)).T:
-                        # Get the bin centers
-                        bc = [f"{bc[i]:0.2f}" for bc, i in zip(bcs, inds)]
-                        # Add on the data value
-                        value = d[tuple(inds)]
-                        print(inds, d.shape, value)
-                        bc.append(f"{value:0.3f}")
-                        output_string.append("ROW: " + "  ".join(bc))
-                    output_string.append("ENDMAP:\n")
-            with open(f"maps/DES5YR_{n}_ERRORFUDGES_DIFFIMG_{'_'.join(names)}_{cut}.DAT", "w") as ff:
-                ff.write("\n".join(output_string))
+                        output_string.append("MAPNAME: FLUXERR_SCALE")
+                        output_string.append(f"BAND: {b}  FIELD: {field}")
+                        output_string.append(f"VARNAMES:  {' '.join(names)} ERRSCALE")
+                        for inds in np.indices(d.shape).reshape((len(d.shape), -1)).T:
+                            # Get the bin centers
+                            bc = [f"{bc[i]:0.2f}" for bc, i in zip(bcs, inds)]
+                            # Add on the data value
+                            value = d[tuple(inds)]
+                            print(inds, d.shape, value)
+                            bc.append(f"{value:0.3f}")
+                            output_string.append("ROW: " + "  ".join(bc))
+                        output_string.append("ENDMAP:\n")
+                with open(f"maps/DES5YR_{n}_ERRORFUDGES_DIFFIMG_{'_'.join(names)}_{cut}_{int(cut_fitprob)}.DAT", "w") as ff:
+                    ff.write("\n".join(output_string))
