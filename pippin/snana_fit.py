70cfa88a8a3778e85a052337a13a5081c2d76836
Samuel Hinton <samuelreay@gmail.com>
2020-05-06 16:12:42 +1000

    Testing external lcfit

--- pippin/snana_fit.py
+++ pippin/snana_fit.py
@@ -58,6 +58,7 @@ class SNANALightCurveFit(ConfigBasedExecutable):
         self.num_empty_threshold = 20  # Damn that tarball creation can be so slow
         self.display_threshold = 8
         self.output["fitres_dirs"] = self.fitres_dirs
+        self.output["base_file"] = self.base_file
         self.output["nml_file"] = self.config_path
         self.output["genversion"] = self.sim_version
         self.output["sim_name"] = sim_task.output["name"]
