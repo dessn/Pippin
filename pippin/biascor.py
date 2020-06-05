f18ebf98fb30f10a9c0d818b6a52ae7392a13a92
Samuel Hinton <samuelreay@gmail.com>
2020-06-05 13:10:30 +1000

    Removing file

--- pippin/biascor.py
+++ pippin/biascor.py
@@ -204,6 +204,7 @@ class BiasCor(ConfigBasedExecutable):
             self.set_property("INPDIR+", None, assignment=": ")
             # TODO: Find best way of checking for ranseed change as well and abort
             self.set_property("datafile", ",".join(self.data_fitres), assignment="=", section_end="STRINGMATCH_IGNORE")
+            self.set_property("file", None, assignment="=")
         else:
             bullshit_hack = ""
             for i, d in enumerate(self.data):
