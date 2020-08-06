f8bf63dad88a708d2952da730e80ab3beeede33f
Samuel Hinton <samuelreay@gmail.com>
2020-08-06 10:33:04 +1000

    Making the config filenames for BBC inputs the same as the bbc name

--- pippin/biascor.py
+++ pippin/biascor.py
@@ -44,7 +44,7 @@ class BiasCor(ConfigBasedExecutable):
         self.num_verions = [len(m.output["fitres_dirs"]) for m in self.merged_data]
         self.genversion = "_".join(self.sim_names) + ("" if self.classifier is None else "_" + self.classifier.name)
 
-        self.config_filename = f"BBC_{self.name}_{self.genversion}.input"  # Make sure this syncs with the tmp file name
+        self.config_filename = f"{self.name}.input"  # Make sure this syncs with the tmp file name
         self.config_path = os.path.join(self.output_dir, self.config_filename)
         self.job_name = os.path.basename(self.config_path)
         self.fit_output_dir = os.path.join(self.output_dir, "output")
