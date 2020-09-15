2a28cb03bd04f578c844f7ba9dab36ae628c75bc
Samuel Hinton <samuelreay@gmail.com>
2020-09-15 17:29:28 +1000

    Adding SUBMIT.INFO to what is copied manually during merging

--- pippin/merge.py
+++ pippin/merge.py
@@ -159,7 +159,7 @@ class Merger(Task):
                             os.symlink(os.path.join(s[1], "FITOPT000.FITRES.gz"), os.path.join(s[1], s[2]))
 
                     self.logger.debug(f"Copying MERGE.LOG and FITOPT.README")
-                    filenames = ["MERGE.LOG", "FITOPT.README"]
+                    filenames = ["MERGE.LOG", "FITOPT.README", "SUBMIT.INFO"]
                     for f in filenames:
                         original = os.path.join(self.lc_fit["lc_output_dir"], f)
                         moved = os.path.join(self.suboutput_dir, f)
