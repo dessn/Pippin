267e47b866a24778df03d794c8e037a53c861486
Samuel Hinton <samuelreay@gmail.com>
2020-11-18 10:51:07 +1000

    Silly Sam

--- pippin/external/create_covariance.py
+++ pippin/external/create_covariance.py
@@ -224,8 +224,8 @@ def get_cov_from_covopt(covopt, contributions, base, calibrators):
                 # If we have calibrators and this is a VPEC term, filter out calib
                 if calibrators and (apply_filter(fitopt_label, "+VPEC") or apply_filter(muopt_label, "+VPEC")):
                     cov2 = cov.copy()
-                    cov2.loc[mask_calib, :] = 0
-                    cov2.loc[:, mask_calib] = 0
+                    cov2[mask_calib, :] = 0
+                    cov2[:, mask_calib] = 0
                     final_cov += cov2
                 else:
                     final_cov += cov
