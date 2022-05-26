cdfce19fcf55e8633c42f7e09ab43772ee8a4bfc
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2022-05-26 15:30:38 -0500

    Trying to fix the docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Samuel Hinton, Patrick Armstrong, Dillon Brout, et. al.'
 
 # -- General configuration ---------------------------------------------------
 
-root_doc = "source/index"
+root_doc = "index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -52,4 +52,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['source/_static']
+html_static_path = ['_static']
